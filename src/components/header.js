e271425e1919ab19d8b41fccc179680fe48165ad
meghabalaraju <kbmegha4@outlook.com>
2021-01-16 20:33:56 +0100

    navbar added for mobile and desktop version

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,45 @@
 import React from "react"
 import Sitelogo from "../svg/sitelogo.svg"
+import { styled } from "@material-ui/core/styles"
+import { Container, Box, makeStyles } from "@material-ui/core"
+import TextButton from "./uicomponents/uiButtons/textButton"
+
+const FlexBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: 10,
+
+  // media query using Mui
+  [theme.breakpoints.down("xs")]: {
+    flexDirection: "column",
+    textAlign: "center",
+  },
+}))
+
+const useStyles = makeStyles({
+  uMarignBottom: {
+    marginBottom: 20,
+    marginTop: 20,
+  },
+})
 
 const Header = () => {
+  const classes = useStyles()
   return (
-    <div>
-      <Sitelogo />
-    </div>
+    <>
+      <Container maxWidth="lg">
+        <FlexBox className={classes.uMarignBottom}>
+          <Sitelogo />
+          <div>
+            <TextButton>About Me</TextButton>
+            <TextButton>Projects</TextButton>
+          </div>
+        </FlexBox>
+      </Container>
+    </>
   )
 }
 
