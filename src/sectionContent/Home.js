8101197d80c1ca6ed49b6b127484f8669e65fb73
meghabalaraju <kbmegha4@outlook.com>
2021-01-19 20:11:38 +0100

    icon button component added along with the icons that will be used in the site also added

--- src/sectionContent/Home.js
+++ src/sectionContent/Home.js
@@ -1,17 +1,19 @@
 import React from "react"
-import { Container, Box } from "@material-ui/core"
+import { Box } from "@material-ui/core"
 import { styled } from "@material-ui/core/styles"
 import { useStaticQuery, graphql } from "gatsby"
 import Responsive from "../svg/responsive.svg"
+import PortIconButton from "../components/uicomponents/uiButtons/iconButton"
+import { IoLogoGithub, IoDocumentTextOutline } from "react-icons/io5"
+import { GrLinkedinOption } from "react-icons/gr"
+import { HiOutlineMail } from "react-icons/hi"
 
 const FlexBox = styled(Box)(({ theme }) => ({
   display: "flex",
   flexDirection: "row",
-  flexWrap: "wrap",
-  alignItems: "center",
+  flexWrap: "nowrap",
   justifyContent: "space-between",
   padding: 10,
-  width: "100%",
 
   // media query using Mui
   [theme.breakpoints.down("xs")]: {
@@ -46,26 +48,30 @@ const Home = () => {
     }
   `)
   return (
-    <Container maxWidth="lg">
-      <FlexBox>
-        <div>
-          <h1>Hi, I'm {data.contentfulPortfolio.firstname}!</h1>
-          <p>
-            {
-              data.contentfulPortfolio.personalityDescription
-                .personalityDescription
-            }
-          </p>
-          <h3>{data.contentfulPortfolio.contact.github}</h3>
-          <h3>{data.contentfulPortfolio.contact.linkedin}</h3>
-          <h3>{data.contentfulPortfolio.contact.mail}</h3>
-        </div>
-
-        <div>
-          <Responsive />
-        </div>
-      </FlexBox>
-    </Container>
+    <FlexBox>
+      <div style={{ flex: `0 1 35%` }}>
+        <h1>Hi, I'm {data.contentfulPortfolio.firstname}!</h1>
+        <p>
+          {
+            data.contentfulPortfolio.personalityDescription
+              .personalityDescription
+          }
+        </p>
+        <PortIconButton>
+          <IoLogoGithub />
+        </PortIconButton>
+        <PortIconButton>
+          <GrLinkedinOption />
+        </PortIconButton>
+        <PortIconButton>
+          <HiOutlineMail />
+        </PortIconButton>
+        <PortIconButton>
+          <IoDocumentTextOutline />
+        </PortIconButton>
+      </div>
+      <Responsive style={{ flex: `0 1 40%` }} />
+    </FlexBox>
   )
 }
 
