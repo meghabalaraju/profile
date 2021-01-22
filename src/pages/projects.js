153cdecffee91f7e057ed6b041cf90d074cbf363
meghabalaraju <kbmegha4@outlook.com>
2021-01-22 13:02:34 +0100

    Projects page initial setup

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
+import ContainedBtn from "../components/uicomponents/uiButtons/containedButton"
 
 const ProjectPage = () => (
   <Layout>
     <SEO title="Projects" />
     <h1>Projects</h1>
+    <ContainedBtn>COMING SOON</ContainedBtn>
   </Layout>
 )
 
